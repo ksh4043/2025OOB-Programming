d7ddcdde2f4bf5ea8052e02dd21916a50cdfa6dc
ksh4043 <rlatlgus925@naver.com>
2025-03-11 09:04:12 +0900

    First Week OOB(Java)-Programming Class

---
+++ week01/src/week01/HelloWorld.java
@@ -0,0 +1,11 @@
+package week01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("학과 : 컴퓨터정보과");
+		System.out.println("학번 : 202444091");
+		System.out.println("이름 : 김시현");
+	}
+
+}
