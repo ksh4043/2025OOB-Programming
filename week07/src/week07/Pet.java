4576169ca25386b318cb70a61b1539ee6d523e45
ksh4043 <rlatlgus925@naver.com>
2025-04-17 19:16:45 +0900

    add file

--- week07/src/week07/Pet.java
+++ week07/src/week07/Pet.java
@@ -4,10 +4,26 @@ public class Pet {
 	String name;
 	String type;
 	int age;
+	String color;
+	
+	Pet(String name, String type){
+		this.name = name;
+		this.type = type;
+		this.age = 5;
+		this.color = "미정";
+	}
 	
 	Pet(String name, String type, int age){
 		this.name = name;
 		this.type = type;
 		this.age = age;
+		this.color = "미정";
+	}
+	
+	Pet(String name, String type, int age, String color){
+		this.name = name;
+		this.type = type;
+		this.age = age;
+		this.color = color;
 	}
 }
--- week07/src/week07/PetEx.java
+++ week07/src/week07/PetEx.java
@@ -2,8 +2,13 @@ package week07;
 
 public class PetEx {
 	public static void main(String[] args) {
-		Pet pet1 = new Pet("바둑이", "강아지", 3);
+		Pet pet1 = new Pet("구름", "강아지", 2, "흰색");
+		Pet pet2 = new Pet("바둑이", "강아지", 3);
+		Pet pet3 = new Pet("초코", "고양이");
+		
 		System.out.println("< 애완동물 정보 >");
-		System.out.printf("이름 : %s\n종류 : %s\n나이 : %d살\n", pet1.name, pet1.type, pet1.age);
+		System.out.printf("이름 : %s\n종류 : %s\n나이 : %d살\n색상 : %s\n", pet1.name, pet1.type, pet1.age, pet1.color);
+		System.out.printf("이름 : %s\n종류 : %s\n나이 : %d살\n색상 : %s\n", pet2.name, pet2.type, pet2.age, pet2.color);
+		System.out.printf("이름 : %s\n종류 : %s\n나이 : %d살\n색상 : %s\n", pet3.name, pet3.type, pet3.age, pet3.color);
 	}
 }
